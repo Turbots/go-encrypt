9c1f779bfea5c013ad604a196026d59c76b68685
Dieter Hubau <dhubau@gmail.com>
2018-07-12 21:19:19 +0200

    Added go routine for watching routes

--- main.go
+++ main.go
@@ -1,16 +1,19 @@
 package main
 
 import (
-	"fmt"
+	"log"
 
 	k8sMetav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	k8sCorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
 	k8sClientCmd "k8s.io/client-go/tools/clientcmd"
 	routev1 "github.com/openshift/api/route/v1"
 	routeClientv1 "github.com/openshift/client-go/route/clientset/versioned/typed/route/v1"
+	"time"
+	"os"
 )
 
 func main() {
+	log.SetOutput(os.Stdout)
+
 	kubeConfig := k8sClientCmd.NewNonInteractiveDeferredLoadingClientConfig(
 		k8sClientCmd.NewDefaultClientConfigLoadingRules(),
 		&k8sClientCmd.ConfigOverrides{},
@@ -21,35 +24,43 @@ func main() {
 		panic(err)
 	}
 
+	log.Println("Go Encrypt - Copyright Ordina JWorks 2018")
+
+	go watchRoutes(namespace, kubeConfig)
+
+	for true {
+		time.Sleep(time.Hour)
+	}
+}
+
+func watchRoutes(namespace string, kubeConfig k8sClientCmd.ClientConfig) {
+	log.Println("Watching for Routes in namespace ", namespace)
+
 	restConfig, err := kubeConfig.ClientConfig()
+
 	if err != nil {
 		panic(err)
-	}
-
-	coreClient, err := k8sCorev1.NewForConfig(restConfig)
-	if err != nil {
-		panic(err)
-	}
-
-	pods, err := coreClient.Pods(namespace).List(k8sMetav1.ListOptions{})
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("Pods in namespace %s:\n", namespace)
-	for _, pod := range pods.Items {
-		fmt.Printf("  %s\n", pod.Name)
 	}
 
 	routeClient, err := routeClientv1.NewForConfig(restConfig)
+
+	if err != nil {
+		panic(err)
+	}
+
 	routeWatch, err := routeClient.Routes(namespace).Watch(k8sMetav1.ListOptions{})
+
+	if err != nil {
+		panic(err)
+	}
 
 	for {
 		select {
 		case event := <-routeWatch.ResultChan():
 			route := event.Object.(*routev1.Route)
 
-			fmt.Println("Route\t", route.Name, "\t", route.Spec.Host)
+			log.Println("Route Event Received")
+			log.Println(route.Name, "\t", route.Spec.Host)
 		}
 	}
 }
